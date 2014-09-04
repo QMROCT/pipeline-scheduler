fc51a4fa6538e755a79ba1c970924cde399e83f3
Christoph Jansen <jansen.christoph@yahoo.de>
2014-09-04 13:22:32 +0200

    install.sh file added

---
+++ install.sh
@@ -0,0 +1,33 @@
+if [[ ! -f "install.sh" ]]; then
+	echo "ERROR: run install.sh from directory containing it"
+	exit 1
+fi
+
+if [[ ! -d PipelineSchedulerServer ]]; then
+	echo "ERROR: current directory must contain python module PipelineSchedulerServer"
+	exit1
+fi
+
+SRC=$(pwd)
+DEST=~/.pipeline-scheduler/
+
+# copy configuration templates
+[[ ! -d ${DEST} ]] && mkdir ${DEST}
+cp *.yaml.template ${DEST}
+
+# rename templates but do not override existing
+cd ${DEST}
+for file in *.yaml.template; do
+	name=$(basename ${file} .yaml.template)
+	if [[ ! -f ${name}.yaml ]]; then
+		mv ${file} ${name}.yaml
+	else
+		rm ${file}
+	fi
+done
+
+# add pipeline-scheduler to PYTHONPATH
+echo "PYTHONPATH=\"\${PYTHONPATH}:${SRC}/PipelineSchedulerServer\"" >> ~/.bashrc
+echo "export PYTHONPATH" >> ~/.bashrc
+
+source ~/.bashrc

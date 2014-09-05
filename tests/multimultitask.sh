aa9c368be8a48cb1f90eb3aa1a24bfa056b8ea6f
Christoph Jansen <jansen.christoph@yahoo.de>
2014-09-05 15:05:28 +0200

    more tests

--- tests/multimultitask.sh
+++ tests/multimultitask.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-NUM_OF_TESTS=10
+NUM_OF_TESTS=3
 
 for i in $(seq 1 1 ${NUM_OF_TESTS}); do
 	bash /vagrant/pipeline-scheduler/tests/multitask.sh
-	sleep 5000
-	mv /vagrant/pipeline-scheduler/timetest.csv /vagrant/pipeline-scheduler/tests/servers10_ttl1_maxactive1_${i}.csv
+	sleep 400
+	mv /vagrant/pipeline-scheduler/timetest.csv /vagrant/pipeline-scheduler/tests/servers10_ttl1_maxactive1_docker_${i}.csv
 done
 
